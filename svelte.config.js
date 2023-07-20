58d3e765547e8cf7098874448b338d1ecea7c32b
hestrr <moiseeva.polina@gmail.com>
2023-07-20 16:38:02 +0300

    add adapter-cloudflare for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.argv.includes('dev');
@@ -14,9 +14,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/portfolio-website',
-		  },
 	}
 };
 
