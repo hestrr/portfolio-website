dff45eb68bb25b9c02acca94aa0490a5d4f0f811
hestrr <moiseeva.polina@gmail.com>
2023-07-20 16:29:54 +0300

    add SSR with adapter-node

--- src/routes/comic/+page.server.ts
+++ src/routes/comic/+page.server.ts
@@ -1,3 +1,7 @@
+import { formatDistance } from 'date-fns';
+
+const getComicButton = "Get comic";
+
 interface Joke {
     month: string;
     num: number;
@@ -11,16 +15,26 @@ interface Joke {
     title: string;
     day: string;
 }
-
 async function fetchComicId(emailParam: string) {
     const params = new URLSearchParams({email: emailParam});
     return (await fetch('https://fwd.innopolis.app/api/hw2?' + params.toString())).json()
 }
 
-export async function getJoke(): Promise<Joke> {
+async function getJoke(): Promise<Joke> {
     const comicId = await fetchComicId("p.moiseeva@innopolis.university");
     const params = new URLSearchParams({id: comicId});
     const link = 'https://fwd.innopolis.university/api/comic?' + params.toString();
 
     return (await fetch(link)).json()
 }
+
+export async function load() {
+    return {props: await getJoke()}
+}
+
+function getTitleWithDate(data : {props : Joke}) : string {
+    const date = formatDistance(new Date(parseInt(data.props.year), parseInt(data.props.month), parseInt(data.props.day)), new Date()) as string;
+    return data.props.safe_title + ", " + date;
+}
+
+export const ssr = true;
--- src/routes/comic/+page.svelte
+++ src/routes/comic/+page.svelte
@@ -1,81 +1,88 @@
 <script lang="ts">
-	import { formatDistance } from 'date-fns';
+    import { formatDistance } from 'date-fns';
+	import { get } from 'svelte/store';
 
-	function fetchComicId(emailParam: string) {
-		const params = new URLSearchParams({ email: emailParam });
-		return fetch('https://fwd.innopolis.app/api/hw2?' + params.toString()).then((response) =>
-			response.json()
-		);
-	}
+    interface Joke {
+        month: string;
+        num: number;
+        link: string;
+        year: string;
+        news: string;
+        safe_title: string;
+        transcript: string;
+        alt: string;
+        img: string;
+        title: string;
+        day: string;
+    }
 
-	let src: string;
-	let alt: string;
-	let title: string;
-	let button = 'Fetch comic';
+    async function fetchComicId(emailParam: string) {
+        const params = new URLSearchParams({email: emailParam});
+        return (await fetch('https://fwd.innopolis.app/api/hw2?' + params.toString())).json()
+    }
 
-	async function fetchComic() {
-		const comicId = await fetchComicId('p.moiseeva@innopolis.university');
-		const params = new URLSearchParams({ id: comicId });
-		const link = 'https://fwd.innopolis.university/api/comic?' + params.toString();
-		fetch(link)
-			.then((response) => response.json())
-			.then((result) => {
-				src = result.img;
-				alt = result.alt;
-				const date = formatDistance(
-					new Date(result.year, result.month, result.day),
-					new Date()
-				) as string;
-				title = result.safe_title + ', ' + date;
-				button = 'Fetch another one';
-			})
-			.catch((err) => console.log(err));
-	}
+    async function getJoke(): Promise<Joke> {
+        const comicId = await fetchComicId("p.moiseeva@innopolis.university");
+        const params = new URLSearchParams({id: comicId});
+        const link = 'https://fwd.innopolis.university/api/comic?' + params.toString();
+        return (await fetch(link)).json()
+    }
+
+    function getTitleWithDate(data : {props : Joke}) : string {
+    const date = formatDistance(new Date(parseInt(data.props.year), parseInt(data.props.month), parseInt(data.props.day)), new Date()) as string;
+    return data.props.safe_title + ", " + date;
+}
+
+    export async function load() {
+        return {props: await getJoke()}
+    }
+
+    export let data;
 </script>
 
 <div class="container2">
-	<div class="comic-button-cell">
-		<button id="fetch-comic-button" on:click={() => fetchComic()}>{button}</button>
-	</div>
-	<div class="comic-cell">
-		<p class="regular-text" id="comic-title" style="font-weight: bold;">{title}</p>
-		<img id="comic-container" {src} {alt} />
-	</div>
+    <div class="comic-button-cell">
+        <button id="fetch-comic-button" on:click={async () => data=await load()}>Get another comic</button>
+    </div>
+    <div class="comic-cell">
+        <p class="regular-text" id="comic-title" style="font-weight: bold;">{getTitleWithDate(data)}</p>
+        <img id="comic-container" src={data.props.img} alt={data.props.alt}/>
+    </div>
 </div>
 
 <style>
-	.container2 {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-template-rows: 1fr 0.5fr 1fr 1fr;
-		column-gap: 5vmin;
-		grid-template-areas:
+    .container2 {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 0.5fr 1fr 1fr;
+        column-gap: 5vmin;
+        grid-template-areas:
 			'. . .'
 			'. comic-button .'
 			'. comic .'
 			'. . .';
-		height: 100vmin;
-	}
+        height: 100vmin;
+    }
 
-	.comic-cell {
-		padding: 30px 20px 0 20px;
-		grid-area: comic;
-	}
+    .comic-cell {
+        padding: 30px 20px 0 20px;
+        grid-area: comic;
+    }
 
-	.container2 {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-template-rows: 1fr 0.5fr 1fr 1fr;
-		column-gap: 5vmin;
-		grid-template-areas:
+    .container2 {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 0.5fr 1fr 1fr;
+        column-gap: 5vmin;
+        grid-template-areas:
 			'. . .'
 			'. comic-button .'
 			'. comic .'
 			'. . .';
-		height: 100vmin;
-	}
+        height: 100vmin;
+    }
 
-	.comic-button-cell {
-		grid-area: comic-button;
-	}
+    .comic-button-cell {
+        grid-area: comic-button;
+    }
 </style>
